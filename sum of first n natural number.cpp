5d3206a40bd4a8339d8c48531171af8bbe6740c0
chaitanya091 <chaitanyapatilcr7@gmail.com>
2023-04-04 14:34:57 +0530

    IBH Method

---
+++ sum of first n natural number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	int n;
+//	cin>>n;
+//	cout<<"\n"<<n;
+//	int sum=0;
+//	for(int i=0;i<=n;i++)
+//	{
+//		sum = sum+i;
+//	}
+//	cout<<"\n"<<sum;
+//}
+ds
